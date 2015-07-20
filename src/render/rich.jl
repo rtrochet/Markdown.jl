f3b3f3e9922f5c1d2a3514f5126e27005c99109a
rtrochet <rtrochet@gmail.com>
2015-07-20 16:07:14 -0700

    Add LaTeX MIME type

--- src/render/rich.jl
+++ src/render/rich.jl
@@ -4,6 +4,10 @@ function tohtml(io::IO, m::MIME"text/html", x)
     writemime(io, m, x)
 end
 
+function tohtml(io::IO, m::MIME"text/latex", x)
+    writemime(io, m, x)
+end
+
 function tohtml(io::IO, m::MIME"text/plain", x)
     htmlesc(io, sprint(writemime, m, x))
 end
@@ -21,7 +25,7 @@ end
 # Display infrastructure
 
 function bestmime(val)
-    for mime in ("text/html", "image/svg+xml", "image/png", "text/plain")
+    for mime in ("text/html", "image/svg+xml", "text/latex", "image/png", "text/plain")
         mimewritable(mime, val) && return MIME(symbol(mime))
     end
     error("Cannot render $val to Markdown.")
